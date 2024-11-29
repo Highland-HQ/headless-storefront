cd704d9c7588c9869aef34702cfa31bc479937f0
Parker Freestone <parkerfreestone@gmail.com>
2024-11-29 11:26:12 -0700

    :sparkles: Stuff for our black friday sale...

--- app/conf/SiteSettings.ts
+++ app/conf/SiteSettings.ts
@@ -2,4 +2,5 @@ export const FEATURED_COLLECTION_HANDLE: string = 'november-collection';
 export const FEATURED_COLLECTION_QUERY: string = 'tag:Recommended';
 export const FIRST_FEATURED_PRODUCT_HANDLE: string = 'highland-trucker-hats';
 
-export const SITE_ANNOUNCEMENT_TEXT: string = '';
+export const SITE_ANNOUNCEMENT_TEXT: string =
+  'BLACK FRIDAY SALE - 65% OFF SITEWIDE, APPLIED AT CHECKOUT';
